d4992a596e1190005bf4d0cd17fac365a35be11b
ZhaoXinPei17 <zhaoxinpei17@mails.ucas.ac.cn>
2022-10-27 20:38:34 +0800

    add load_one method

--- fmri_models/smn4_loader.py
+++ fmri_models/smn4_loader.py
@@ -17,11 +17,11 @@ class FmriLoader(Album):
 
     def forward(self, sub):
         '''
-        Return:
+            Return:
             train_fmri: (n_TRs, n_voxels); 
             starts: list, the start index of each story in the fmri_path. len = n_story + 1
         '''
-        voxel_top_path = (self.voxel_top_path).replace("$$", sub)
+        voxel_top_path = (self.fmri_feature_corrs_path).replace("$$", sub)
         train_fmri = torch.tensor([])
         starts = [0]
 
@@ -52,8 +52,31 @@ class FmriLoader(Album):
 
         return train_fmri, starts
 
+    def load_one(self, sub, story: int):
+        '''
+            Return: train_fmri: (n_TRs, n_voxels); 
+        '''
+        voxel_top_path = (self.fmri_feature_corrs_path).replace("$$", sub)
+        
+        if self.voxel_top_num is not None:
+            score = scio.loadmat(voxel_top_path)
+            score = np.squeeze(score['test_corrs'])
+            voxel_index = np.argsort(score)
+            voxel_index = voxel_index[-self.voxel_top_num: ]
+            # print(voxel_index.shape, voxel_index[:10])
+        
+        fmri_file = join(self.fmri_path, sub, f'story_{story}.mat')
+        data = h5py.File(fmri_file, 'r')
+
+        single_fmri = np.array(data['fmri_response'])
+        if self.voxel_top_num is not None:
+                # voxel top
+            single_fmri = single_fmri[:, voxel_index]
+
+        return torch.FloatTensor(single_fmri)
+
 
 if __name__ == '__main__':
     
     fmri_loader = FmriLoader()
-    fmri_loader(sub='02')
+    print((fmri_loader.load_one(sub='02', story=1)).shape)
